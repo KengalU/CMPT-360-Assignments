5f55c1c9f0ede89a64e43b67993800e700972f32
Kevin <usuanlelek@mymacewan.ca>
2026-05-07 22:42:47 -0600

    completed validation test, i think, and basic compilation works

--- Assignment1/userclean.c
+++ Assignment1/userclean.c
@@ -16,10 +16,13 @@
 # define MINLEN 3
 # define VALIDCHARS "abcdefghijklmnopqrstuvwxyz1234567890_"
 
+
+// Validation Helper Functions
 bool is_valid_chars(char *username)
 {
     for (int i = 0; username[i] != '\0'; i++)
     {
+        // char must be lowercase letter, digit, or underscore
         if (!islower(username[i]) && !isdigit(username[i]) && username[i] != '_')
         {
             return false;
@@ -37,29 +40,43 @@ bool is_valid_length(char *username)
     return true;
 }
 
+bool is_not_reserved(char *username)
+{
+    if (strcmp(username, "root") == 0 || strcmp(username, "admin") == 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool is_valid(char *username)
+{
+    if (!is_valid_chars(username)) return false;
+    if (!is_valid_length(username)) return false;
+    if (!is_not_reserved(username)) return false;
+    return true;
+}
+
 int main ()
 {
 
     char username[MAXLEN + 1]; // +1 for \0
 
     fgets(username, sizeof(username), stdin);
-    username[strcspn(username, '\n')] = '\0';
+    username[strcspn(username, "\n")] = '\0';
 
-    printf("username is: %s", username); //test
+    printf("username is: %s\n", username); //test
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (is_valid(username))
+    {
+        printf("%s is clean!", username);
+        return 0;
+    }
+    else
+    {
+        printf("%s is not clean", username);
+        // clean username
+        return 0;
+    }
     return 0;
 }
