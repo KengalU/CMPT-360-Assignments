f5a72cd47497db71a4f50ef2a71a41661ae7b8b3
Kevin <kusuanlele@gmail.com>
2026-05-10 23:11:04 -0600

    final touches

--- Assignment1/README.md
+++ Assignment1/README.md
@@ -3,9 +3,30 @@ Name: Kevin Usuanlele / Scott Weaver
 ID:  3103649 / 3144661
 Date: May 7, 2026
 # 1. Academic Integrity
+We certify that this submission represents entirely our own work.
 # 2. Overview
+A command-line utility that reads a username from standard input and checks
+if its meets a set of validity criteria. If the username is valid, it reports
+as such, if not, the program attempts to fix the username using predefined rules.
+If no valid transformation is possible, the program deems the input as invalid
+and unfixable
 # 3. Build
+make - compiles userclean
+make vg - runs valgrind
+make check - runs sample inputs
+make clean - removes build
 # 4. Run
+/
 # 5. Logic Summary
+- Reads username
+- If reserved label as unfixable
+- Otherwise clean:
+    - convert to lowercase
+    - replace spaces with underscores
+    - remove invalid characters
+    - pad/trun to be between 3 and 16 characters
+- Validate again
 # 6. Status
+userclean runs correctly and passes input and valgrind tests.
+Outputs exactly match specifications
 # 6. Notes / Assumptions (Optional)
--- Assignment1/userclean.c
+++ Assignment1/userclean.c
@@ -1,10 +1,10 @@
 /* -----------------------------------------
     Contributors: Kevin Usuanlele / Scott Weaver
     ID's: 3103649 / 3144661
-    Date: May 9, 2026
+    Submission Date: May 10, 2026
     Program: userclean.c
-    Description:
-    Reference(s):
+    Description: Transforms a given username to suitable name that follows the given criteria
+    Reference(s): strcpsn() - https://www.w3schools.com/c/ref_string_strcspn.php
 ------------------------------------------*/
 #include <stdio.h>
 #include <stdbool.h>
@@ -22,13 +22,20 @@
 // Validation Helper Functions
 bool is_valid_first(char *username)
 /*
-    Checks if the first letter is lowercase
+    Purpose: Checks if the first letter is lowercase/digits/underscore
+    Params: username: username string to check
+    Return: True if valid, False otherwise
 */
 {
     return islower(username[0]);
 }
 
 bool is_valid_chars(char *username)
+/*
+    Purpose: Checks if all letters are lowercase/digits/underscore
+    Params: username: username string to check
+    Return: True if all chars are valid, False otherwise
+*/
 {
     for (int i = 0; username[i] != '\0'; i++)
     {
@@ -42,6 +49,11 @@ bool is_valid_chars(char *username)
 }
 
 bool is_valid_length(char *username)
+/*
+    Purpose: Checks if username length is between MINLEN and MAXLEN
+    Params: username: username string to check
+    Return: True if length is valid, False otherwise
+*/
 {
     if (strlen(username) < MINLEN || strlen(username) > MAXLEN)
         {
@@ -50,7 +62,12 @@ bool is_valid_length(char *username)
     return true;
 }
 
-bool is_reserved(char *username) // Checks if the username is "root" or "admin"
+bool is_reserved(char *username)
+/*
+    Purpose: Checks if username is reserved sys name
+    Params: username: username string to check
+    Return: True if reserved, False otherwise
+*/
 {
     if (strcmp(username, "root") == 0 || strcmp(username, "admin") == 0)
     {
@@ -60,6 +77,12 @@ bool is_reserved(char *username) // Checks if the username is "root" or "admin"
 }
 
 bool clean(char *username)
+/*
+    Purpose: Attempts to clean username to fix requirements
+             lowercase -> spaces -> invalid chars -> pad/trun -> validate
+    Params: username: username string to check
+    Return: True if username fix passes validations, False if unfixable
+*/
 {
     if (!is_valid_chars(username)) // bring username to lowercase, replace whitespace with underscore
     {
@@ -72,7 +95,7 @@ bool clean(char *username)
             username[i] = '_';
             }
 
-            if (strchr(VALIDCHARS, username[i]) != NULL) // if username[i] is a valid char
+            if (strchr(VALIDCHARS, username[i]) != NULL) // strchr points to NULL if invalid char
             {
                 username[j] = username[i]; // write
                 j++; // increment j for every valid char
@@ -86,6 +109,7 @@ bool clean(char *username)
     {
         int i = 0;
         while (strlen(username) < MINLEN)
+        // append fillerchars until length req is met
         {
             int end = strlen(username);
             username[end] = FILLERCHARS[i];
@@ -93,6 +117,7 @@ bool clean(char *username)
             i++;
         }
 
+        // cut off string at max length with \0
         if (strlen(username) > MAXLEN)
         {
             username[MAXLEN] = '\0';
@@ -111,16 +136,17 @@ int main ()
 {
     char username[BUFFER], *old_username, *new_username;
 
-    // Get username
+    // Read username
     if (fgets(username, sizeof(username), stdin) == NULL)
     {
         return 1;
     }
-    username[strcspn(username, "\n")] = '\0';
+    username[strcspn(username, "\n")] = '\0'; // replace returned pointer ('\n') with \0
 
     // Alloc mem and save a copy
-    old_username = malloc(strlen(username) + 1); // alloc mem and save original username
-    new_username = malloc(BUFFER); // alloc mem and save for clean
+    old_username = malloc(strlen(username) + 1); // save original username for ouput, +1 for \0
+    strcpy(old_username, username);
+    new_username = malloc(BUFFER);
     strcpy(new_username, username);
 
     // Check if username is reserved
@@ -132,7 +158,7 @@ int main ()
         return 0;
     }
 
-    // Clean username
+    // Attempt to clean username
     bool cleaned = clean(new_username);
     if (cleaned)
     {
